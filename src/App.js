1beb0e912a1252704eeb2d72553eb3a1407856a2
davi gerson <dgas1@discente.ifpe.edu.br>
2024-09-16 21:14:52 -0300

    #

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   H1,
@@ -6,15 +6,36 @@ import {
   Image,
   Input,
   InputLabel,
-  ButtonCadaster
-} from './Styles';
+  ButtonCadaster,
+  Arrow,
+  User,
+  Trash
+} from './Styles/Styles';
+
+import image from './assets/trabalho-de-personagem-de-mulher-3d-no-computador.png'
+import arrow from './assets/seta-branca-em-fundo-laranja-minimalista.png'
+import trash from './assets/delete-icon.svg'
 
 export const App = () => {
+  //const users = [];
+
+  //primeiro React hook - useState
+  const [users, setUsers] = useState([]);
+
+  function addNewUser() {
+    setUsers([{
+      id: Math.random(),
+      name: "Nino",
+      age: 1,
+      email: "gNininho@gmail.com",
+      phone: "(81) 97341-7256"
+    }])
+  }
 
   return (
     <>
       <Container>
-        <Image />
+        <Image alt='peoples' src={image} />
         <ContainerItens>
           <H1>Olá </H1>
 
@@ -27,8 +48,22 @@ export const App = () => {
           <InputLabel >Email</InputLabel>
           <Input type="Email" placeholder="Email" />
 
-          <ButtonCadaster>Cadastrar</ButtonCadaster>
+          <InputLabel>Telefone</InputLabel>
+          <Input type="Phone" placeholder="Telefone" />
 
+          <ButtonCadaster onClick={addNewUser}>Cadastrar <Arrow alt="arrow" src={arrow} /></ButtonCadaster><br />
+
+          <ul>
+            {users.map(user => (
+              <User key={user.id}>
+                <p>{user.name}</p>
+                <p>{user.age}</p>
+                <p>{user.email}</p>
+                <p>{user.phone}</p>
+                <button><Trash src={trash} /></button>
+              </User>
+            ))}
+          </ul>
         </ContainerItens>
       </Container>
     </>
