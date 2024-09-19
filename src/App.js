f629729a14087b61ab6477583d963626576e96ff
davi gerson <dgas1@discente.ifpe.edu.br>
2024-09-19 13:58:16 -0300

    adicionado função de deleção de usuarios

--- src/App.js
+++ src/App.js
@@ -28,19 +28,23 @@ export const App = () => {
   const inputEmail = useRef();
   const inputPhone = useRef();
 
-
-
-
   //função que irá criar e adicionar um novo usuario usando o hook useRef
   function addNewUser() {
-    setUsers([...users,{id: Math.random(),
+    setUsers([...users, {
+      id: Math.random(),
       name: inputName.current.value,
       age: inputAge.current.value,
       email: inputEmail.current.value,
       phone: inputPhone.current.value
     }])
-  }
+  };
 
+  //função para deletar usuario
+  function deleteUser(userId) {
+    const deleteUsers = users.filter(user => user.id !== userId);
+
+    setUsers(deleteUsers);
+  }
 
   return (
     <>
@@ -50,19 +54,20 @@ export const App = () => {
           <H1>Olá </H1>
 
           <InputLabel >nome</InputLabel>
-          <Input type="Name" ref={inputName}  placeholder="Nome" />
+          <Input type="Name" ref={inputName} placeholder="Nome" required/>
 
           <InputLabel >Idade</InputLabel>
-          <Input type="Age" ref={inputAge} placeholder="Idade" />
+          <Input type="Age" ref={inputAge} placeholder="Idade" required/>
 
           <InputLabel >Email</InputLabel>
-          <Input type="Email" ref={inputEmail}  placeholder="Email" />
+          <Input type="Email" ref={inputEmail} placeholder="Email" required/>
 
           <InputLabel>Telefone</InputLabel>
-          <Input type="Phone" ref={inputPhone} placeholder="Telefone" />
+          <Input type="Phone" ref={inputPhone} placeholder="Telefone" required/>
 
           <ButtonCadaster onClick={addNewUser}>Cadastrar <Arrow alt="arrow" src={arrow} /></ButtonCadaster><br />
 
+      {/*lista com o mapeamento e retorno dos usuarios na tela */}
           <ul>
             {users.map(user => (
               <User key={user.id}>
@@ -70,7 +75,7 @@ export const App = () => {
                 <p>{user.age}</p>
                 <p>{user.email}</p>
                 <p>{user.phone}</p>
-                <button><Trash src={trash} /></button>
+                <button onClick={() => deleteUser(user.id)}><Trash src={trash} /></button>
               </User>
             ))}
           </ul>
